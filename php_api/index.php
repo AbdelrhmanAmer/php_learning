e23bbddbe5ac7f682feaf22cd1a0685657b12218
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-04-19 17:01:47 +0200

    Creating the Search URL

--- php_api/index.php
+++ php_api/index.php
@@ -1,15 +1,20 @@
 <?php
     echo "Welcome to curl";
     
-    // 1) create cURL resource
     $ch = curl_init();
-    
-    // 2) set cURL options
-    curl_setopt($ch, CURLOPT_URL, 'https://www.amazon.com');
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // https
-    // 3) run cURL (execute http request)
-    $server_response = curl_exec($ch);
+    $search_string = "video+games";
+    $url = "https://www.alibaba.com/trade/search?spm=a2700.product_home_newuser.home_new_user_first_screen_fy23_pc_search_bar.keydown__Enter&tab=all&SearchText=$search_string";
 
-    // 4) close cURL resource
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // https
+    
+    //This option tells cURL to return the fetched web page content as a string
+    // from curl_exec() instead of outputting it directly.
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+    
+    // https://m.media-amazon.com/images/I/71BamMUl0VL._AC_UY218_.jpg
+
+    $server_response = curl_exec($ch);
+    echo $server_response;
     curl_close($ch);
 ?>

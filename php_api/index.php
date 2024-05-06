808efb702b6bab1a1928d69fbc569ab31f81ce9e
AbdelrhmanAmer <12121212aboziad@gmail.com>
2024-05-06 23:11:13 +0300

    Define class

--- php_api/index.php
+++ php_api/index.php
@@ -1,20 +1,19 @@
-<?php
-    echo "Welcome to curl";
-    
-    $ch = curl_init();
-    $search_string = "video+games";
-    $url = "https://www.alibaba.com/trade/search?spm=a2700.product_home_newuser.home_new_user_first_screen_fy23_pc_search_bar.keydown__Enter&tab=all&SearchText=$search_string";
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $ch = curl_init();
 
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // https
-    
-    //This option tells cURL to return the fetched web page content as a string
-    // from curl_exec() instead of outputting it directly.
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-    
-    // https://m.media-amazon.com/images/I/71BamMUl0VL._AC_UY218_.jpg
+            $search_string = "pc+video+games+2016";
+            $url = "https://google.com/";
 
-    $server_response = curl_exec($ch);
-    echo $server_response;
-    curl_close($ch);
-?>
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+            
+            $result_url = curl_exec($ch);
+            echo $result_url;
+
+            curl_close($ch);
+        ?>
+    </body>
+</html>
